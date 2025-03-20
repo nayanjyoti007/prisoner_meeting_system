cf644f3402c080b8bc6bfc2c3124a9121cb53116
nayanjyoti007 <work.nayanjyotikalita@gmail.com>
2025-03-20 16:16:53 +0530

    nayan

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="{{ asset('assam_police_logo.png') }}">
     <title>Assam State Inmate Visitation System || Control Panel Login</title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/visitor/login.blade.php
+++ resources/views/visitor/login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prisoner Meeting System</title>
+    <meta property="og:image" content="{{ asset('assam_police_logo.png') }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Google Fonts: Lato -->
